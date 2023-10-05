34ad6b95b9a6405dbb2e8493ef7dd025cba50738
Aric-prog <hernandoaric@gmail.com>
2023-10-05 14:45:09 +1100

    initial frontend

--- room_booking/frontend/urls.py
+++ room_booking/frontend/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, my_reservation, reserve
 
-urlpatterns = [path("", index, name="Enrollments")]
+urlpatterns = [
+    path("", index, name="Enrollments"),
+    path("reserve/", reserve),
+    path("my-reservation", my_reservation),
+]
--- room_booking/frontend/views.py
+++ room_booking/frontend/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "frontend/index.html")
+
+def my_reservation(request):
+    return render(request, "frontend/my-reservation/index.html")
+
+def reserve(request):
+    return render(request, "frontend/reserve/index.html")
