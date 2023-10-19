249b18998acea3daaa813b9ea7f6671ed3f24c9f
Ozy-Viking <admin@hankin.io>
2023-10-19 22:02:58 +1100

    Header home name change, added dockerfile

--- room_booking/templates/src/components/Header.tsx
+++ room_booking/templates/src/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header({ title, memberId }: Props) {
       <div className="md:flex menu text-white h-full flex flex-col md:flex-row items-center justify-center md:mx-4">
         <div className="px-4 ease-in-out duration-150 text-center cursor-pointer py-1 border-white border-y md:border-0 border-x-0 w-full md:w-fit">
           <a className="font-jakarta my-5" href="/library">
-            Home
+            Room Bookings
           </a>
         </div>
         <MyReservation memberId={memberId}></MyReservation>
